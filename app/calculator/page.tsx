cb7ce67b3c715f82749fc6b4130f2f6589be50c6
breezyChedi <165708294+breezyChedi@users.noreply.github.com>
2024-11-26 17:23:06 +0200

    Update page.tsx

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -399,7 +399,7 @@ const CalculatorPage: React.FC = () => {
                     variant="contained"
                     color="primary"
                     href="https://nbtests.uct.ac.za/"
-                    style={{ marginTop: '16px', marginRight: '5px',marginLeft: '5px' }}
+                    style={{ marginTop: '16px', marginRight: '5px',marginLeft: '5px', justifyContent: 'center' }}
                 >
                     Book a NBT test
                 </Button>
