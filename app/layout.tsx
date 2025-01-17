83bdbfbe0e3e4553a5463e1ab1495c2ca1f5b616
breezyChedi <165708294+breezyChedi@users.noreply.github.com>
2025-01-17 16:31:51 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,7 +101,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                   marginRight: '56px',
                 }}
               >
-                Cherry Web App
+                Cherry
               </Typography>
 
               {/* Right-side Icon */}
